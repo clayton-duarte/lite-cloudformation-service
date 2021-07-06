b5da8a8015b7807a0e45a75d5973104a85564730
Clayton Duarte <clayton@null.net>
2021-07-06 14:14:36 -0400

    :tada: Unzip lambda ready!

--- lib/lite-cloudformation-service-stack.ts
+++ lib/lite-cloudformation-service-stack.ts
@@ -1,5 +1,5 @@
+import { S3EventSource } from '@aws-cdk/aws-lambda-event-sources'
 import * as LambdaNodeJs from '@aws-cdk/aws-lambda-nodejs'
-import * as ApiGateway from '@aws-cdk/aws-apigateway'
 import * as S3 from '@aws-cdk/aws-s3'
 import * as CDK from '@aws-cdk/core'
 
@@ -7,67 +7,36 @@ export class LiteCloudformationServiceStack extends CDK.Stack {
   constructor(scope: CDK.Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props)
 
-    // Buckets
-    const zippedThemesBucket = new S3.Bucket(this, 'LiteThemesZipped', {
+    // Themes Bucket
+    const themesBucket = new S3.Bucket(this, 'lite-themes', {
       removalPolicy: CDK.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       versioned: true,
     })
-
-    const unzippedThemesBucket = new S3.Bucket(this, 'LiteThemesUnzipped', {
-      removalPolicy: CDK.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      versioned: true,
-    })
-
-    // Api
-    const api = new ApiGateway.RestApi(this, 'tb-lite-serverless-service', {
-      restApiName: 'tb-lite-serverless-service',
-      description: 'Lite Serverless RestAPI',
-    })
-
-    // List Lambda
-    const listObjectsHandler = new LambdaNodeJs.NodejsFunction(
-      this,
-      'listObjects',
-      {
-        entry: 'lambdas/index.ts',
-        handler: 'listObjectsHandler',
-        environment: {
-          BUCKET_UNZIP_NAME: unzippedThemesBucket.bucketName,
-          BUCKET_ZIP_NAME: zippedThemesBucket.bucketName,
-        },
-      }
-    )
-
-    unzippedThemesBucket.grantRead(listObjectsHandler)
-    zippedThemesBucket.grantRead(listObjectsHandler)
-
-    const getListObjects = new ApiGateway.LambdaIntegration(listObjectsHandler)
-    const listApi = api.root.addResource('list')
-    listApi.addMethod('GET', getListObjects)
 
     // Unzip Lambda
     const unzipObjectHandler = new LambdaNodeJs.NodejsFunction(
       this,
       'unzipObject',
       {
-        // TODO: 10s might be too much or too little
+        description: 'Lambda function to un-zip files',
+        // TODO: 10s might be too little (300kb == 5~8s) :shrug:
         timeout: CDK.Duration.seconds(10),
         handler: 'unzipObjectHandler',
         entry: 'lambdas/index.ts',
         environment: {
-          BUCKET_UNZIP_NAME: unzippedThemesBucket.bucketName,
-          BUCKET_ZIP_NAME: zippedThemesBucket.bucketName,
+          BUCKET_NAME: themesBucket.bucketName,
         },
       }
     )
 
-    unzippedThemesBucket.grantReadWrite(unzipObjectHandler)
-    zippedThemesBucket.grantRead(unzipObjectHandler)
+    themesBucket.grantReadWrite(unzipObjectHandler)
 
-    const getUnzipObject = new ApiGateway.LambdaIntegration(unzipObjectHandler)
-    const unzipApi = api.root.addResource('unzip')
-    unzipApi.addMethod('GET', getUnzipObject)
+    unzipObjectHandler.addEventSource(
+      new S3EventSource(themesBucket, {
+        events: [S3.EventType.OBJECT_CREATED],
+        filters: [{ suffix: '.zip' }],
+      })
+    )
   }
 }
