7131da9611f40e747bf68aefd1100964bf3b0476
Clayton Duarte <clayton@null.net>
2021-07-06 14:14:36 -0400

    :tada: Unzip lambda ready!

--- lambdas/index.ts
+++ lambdas/index.ts
@@ -1,86 +1,47 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
+import { S3Handler } from 'aws-lambda'
 import * as unzipper from 'unzipper'
 import * as AWS from 'aws-sdk'
 
 const S3 = new AWS.S3()
 
-const unzippedBucketName = process.env.BUCKET_UNZIP_NAME || ''
-const zippedBucketName = process.env.BUCKET_ZIP_NAME || ''
+const BUCKET_NAME = process.env.BUCKET_NAME || ''
 
-export const listObjectsHandler: APIGatewayProxyHandler = async (event) => {
+export const unzipObjectHandler: S3Handler = async (event) => {
   try {
-    const { Contents: UnzipContents }: AWS.S3.ListObjectsV2Output =
-      await S3.listObjectsV2({ Bucket: unzippedBucketName }).promise()
+    // We just grab the first record, we do not accept multiple zip files
+    const objectKey = event.Records[0].s3.object.key
 
-    const { Contents: ZipContents }: AWS.S3.ListObjectsV2Output =
-      await S3.listObjectsV2({ Bucket: zippedBucketName }).promise()
-
-    if (typeof UnzipContents === 'undefined') {
-      throw Error('AWS.S3.ObjectList of UnzipContents is undefined')
-    }
-
-    if (typeof ZipContents === 'undefined') {
-      throw Error('AWS.S3.ObjectList of ZipContents is undefined')
-    }
-
-    return {
-      body: JSON.stringify({
-        zip: ZipContents.map((obj) => obj.Key),
-        unzip: UnzipContents.map((obj) => obj.Key),
-      }),
-      statusCode: 200,
-      headers: {},
-    }
-  } catch (error) {
-    return {
-      body: JSON.stringify(error.stack ?? JSON.stringify(error, null, 2)),
-      statusCode: 500,
-      headers: {},
-    }
-  }
-}
-
-// TODO: make it triggered by S3 put, not by Api
-export const unzipObjectHandler: APIGatewayProxyHandler = async (event) => {
-  try {
-    // const filename = event.queryStringParameters?.filename
-    const filename = 'theme.zip'
-
-    if (filename === undefined) {
-      throw Error('Missing filename query parameter')
-    }
-
-    const fileStream = S3.getObject({ Bucket: zippedBucketName, Key: filename })
+    // then we extract all files on eam
+    const zipFileStream = S3.getObject({ Bucket: BUCKET_NAME, Key: objectKey })
       .createReadStream()
       .pipe(unzipper.Parse({ forceStream: true }))
-      .on('error', (e: any) => console.log(`Error extracting file: `, e))
 
-    // TODO: Logs
-    // TODO: Parallel upload? Promise.all([])?
-    for await (const entry of fileStream) {
-      const target = `extracted/${filename.slice(0, -4)}/${entry.path}`
+    const uploadPromises = []
+
+    for await (const entry of zipFileStream) {
+      const extractedFilesFolder = 'extracted'
+      const zipFileFolder = objectKey.slice(0, -4)
+      const filePath = entry.path
 
       if (entry.type === 'File') {
-        await S3.upload({
-          ContentLength: entry.vars.uncompressedSize,
-          Bucket: unzippedBucketName,
-          Key: target,
-          Body: entry,
-        }).promise()
+        // If file, prepare to upload
+        uploadPromises.push(
+          S3.upload({
+            Key: [extractedFilesFolder, zipFileFolder, filePath].join('/'),
+            ContentLength: entry.vars.uncompressedSize,
+            Bucket: BUCKET_NAME,
+            Body: entry,
+          }).promise()
+        )
+      } else {
+        // if not, remove from stream
+        // https://github.com/ZJONSSON/node-unzipper#parse-zip-file-contents
+        entry.autodrain()
       }
     }
 
-    // TODO: better return body
-    return {
-      body: 'OK',
-      statusCode: 200,
-      headers: {},
-    }
+    await Promise.all(uploadPromises)
   } catch (error) {
-    return {
-      body: JSON.stringify(error.stack ?? JSON.stringify(error, null, 2)),
-      statusCode: 500,
-      headers: {},
-    }
+    // TODO: log to cloudwatch?
   }
 }
